69ab53a61d3f173f31218a516852c7de33bfe203
GARCIA <sgarcia340@cnm.edu>
2020-09-23 19:11:29 -0600

    Add project files.

---
+++ Garcia_P2_EllipseGenerator/wwwroot/JSSourceFile.js
@@ -0,0 +1,14 @@
+﻿//function setup {
+//   createCanvas(400, 400);
+//}
+//function draw(num1, num2, num3, num4) {
+//    background(220);
+//    fill('yellow');
+//    // An ellipse 
+//    //ellipse(num1, num2, num3, num4)
+
+//    // Draw the ellipse
+//    ctx.beginPath();
+//    ctx.ellipse(num1, num2, num3, num4, Math.PI / 4, 0, 2 * Math.PI);
+//    ctx.stroke();
+//}  
